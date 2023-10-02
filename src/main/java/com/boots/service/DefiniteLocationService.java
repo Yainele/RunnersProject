872ba833ea7e41dc2af30f54a1765e24c5896e8d
Serio <SeriousNuts@yandex.ru>
2023-10-03 00:31:44 +0700

    https://stackoverflow.com/questions/17860696/not-allowed-to-create-transaction-on-shared-entitymanager-use-spring-transacti прочитай

--- src/main/java/com/boots/service/DefiniteLocationService.java
+++ src/main/java/com/boots/service/DefiniteLocationService.java
@@ -3,6 +3,7 @@ package com.boots.service;
 import com.boots.event.DefiniteLocation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,22 +18,24 @@ public class DefiniteLocationService {
         return em.createQuery("select l from DefiniteLocation l", DefiniteLocation.class)
                 .getResultList();
     }
-
+    @Transactional
     public Object createCustomLocation(DefiniteLocation definiteLocation){
         if (definiteLocation == null){
             return false;
         }
-        em.createQuery("INSERT INTO public.t_role(id," +
-                " locationName," +
-                "locationDescription," +
-                "longitude," +
-                "latitude," +
-                "locationStatus", com.boots.event.DefiniteLocation.class)
-                .setParameter("locationName",definiteLocation.getLocationName())
-                .setParameter("locationDescription",definiteLocation.getLocationDescription())
-                .setParameter("longitude",definiteLocation.getLongitude())
-                .setParameter("latitude",definiteLocation.getLatitude())
-                .setParameter("locationStatus",definiteLocation.getLocationStatus());
+        em.getTransaction().begin();
+        em.persist(definiteLocation);
+        em.flush();
+        em.getTransaction().commit();
+//        em.createQuery("INSERT into public.t_DefiniteLocation (id,locationName,locationDescription,longitude,latitude,locationStatus) VALUES (:id, :location_name, :locationDescriptio,:longitude, :latitude, :locationStatus)"
+//                        )
+//                .setParameter("id", definiteLocation.getId())
+//                .setParameter("locationName",definiteLocation.getLocationName())
+//                .setParameter("locationDescription",definiteLocation.getLocationDescription())
+//                .setParameter("longitude",definiteLocation.getLongitude())
+//                .setParameter("latitude",definiteLocation.getLatitude())
+//                .setParameter("locationStatus",definiteLocation.getLocationStatus())
+//                .executeUpdate();
 
         return true;
     }
